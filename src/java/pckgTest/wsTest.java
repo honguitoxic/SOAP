1120e81c776af1248167515cc8737b14aabe9e5c
victor-rodriguez-jd <victor.rodriguez@janeirodigital.com>
2016-04-18 00:02:50 -0600

    Test WS

--- src/java/pckgTest/wsTest.java
+++ src/java/pckgTest/wsTest.java
@@ -5,9 +5,11 @@
  */
 package pckgTest;
 
+import java.text.DecimalFormat;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import static pckgTest.InfixToPostfix.NumberValidator;
 
 /**
  *
@@ -26,5 +28,23 @@ public class wsTest {
     public String substract(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
         int k = i - j;
         return Integer.toString(k);
+    }
+    
+    @WebMethod(operationName = "evaluate")
+    public String evaluate(@WebParam(name = "expr") String expr) throws Exception {
+        expr = expr.replace("'", "");
+        expr = expr.replace("[", "");
+        expr = expr.replace("]", "");
+        InfixToPostfix test = new InfixToPostfix();
+        String[] infix = expr.split(", ");
+        String[] postfix = new String[infix.length];
+        if (NumberValidator(infix)) {
+            test.convert(infix, postfix);
+            DecimalFormat formatter = new DecimalFormat("#,###.00");
+
+            return formatter.format(test.evaluate(postfix));
+        }
+        else
+            throw new Exception("Invalid input");
     }
 }
